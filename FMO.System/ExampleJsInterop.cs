1915238d335204850f629254dc1542bd0096063c
mmichtch <maxim@4refuel.com>
2020-11-11 11:59:29 -0800

    Add project files.

---
+++ FMO.System/ExampleJsInterop.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+
+using Microsoft.JSInterop;
+
+namespace zedIt.Fmo.System
+{
+    public class ExampleJsInterop
+    {
+        public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+        {
+            // Implemented in exampleJsInterop.js
+            return jsRuntime.InvokeAsync<string>(
+                "exampleJsFunctions.showPrompt",
+                message);
+        }
+    }
+}
