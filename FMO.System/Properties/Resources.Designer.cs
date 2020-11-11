58d9ead8b4b0e7dff6be4630439e37fe067c8a82
mmichtch <maxim@4refuel.com>
2020-11-11 13:01:48 -0800

    Simplified project

--- FMO.System/Properties/Resources.Designer.cs
+++ FMO.System/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FMO.System.Properties {
+namespace zedIt.Fmo.System.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FMO.System.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FMO.System.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("zedIt.Fmo.System.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
